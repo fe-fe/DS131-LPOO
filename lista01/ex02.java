fe97d946582f29b86b67551a40187b96ca95b84d
Maria Fernanda <119269054+fe-fe@users.noreply.github.com>
2025-03-27 16:05:17 -0300

    Create ex02.java

---
+++ lista01/ex02.java
@@ -0,0 +1,43 @@
+/*
+E2) Faça um programa que apresente os números de 1 a 15 na forma de um triangulo, como
+apresentado a seguir:
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+Utilize nesse exercício apenas 1 laço
+
+Alteração: permite que o tamanho da pirâmide seja alterado e preenche as casas faltando com X caso a pirâmide não tenha números suficientes para completar a base
+
+*/
+
+
+public class Lista1Ex2 {
+
+    public static void main(String[] args) {
+        
+        int max = 50;
+        int linha = 1;
+        int casa = 0;
+        
+        for (int i = 1; i <= max; i ++) {
+            
+            System.out.print(i + " ");
+            casa++;
+            
+            if (casa == linha) {
+                System.out.println();
+                linha++;
+                casa = 0;
+            } 
+        }
+        
+        if (casa < linha) {   
+            max++;
+            for (; casa < linha; casa++) {
+                System.out.print("X" + " ".repeat((max % 10) + 1));
+            }
+        }
+    }
+}
